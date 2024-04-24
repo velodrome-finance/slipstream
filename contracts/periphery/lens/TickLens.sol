feb73f64399fab2b736b449d76ea1b6ad2ffdc20
Ricardo Airton <58168802+airtoonricardo@users.noreply.github.com>
2024-04-24 10:51:30 +0100

    chore: move getpopulatedticks gas estimator to testticklens (#160)
    * refactor: avoid start index updates when fetching bitmaps
    
    * refactor: move getpopulatedticks with max to testticklens helper
    
    * chore: update gas costs

--- contracts/periphery/lens/TickLens.sol
+++ contracts/periphery/lens/TickLens.sol
@@ -2,11 +2,8 @@
 pragma solidity >=0.5.0;
 pragma abicoder v2;
 
-import {Math} from "@openzeppelin/contracts/math/Math.sol";
-
-import "contracts/core/interfaces/ICLPool.sol";
 import {TickMath} from "../../core/libraries/TickMath.sol";
-
+import "contracts/core/interfaces/ICLPool.sol";
 import "../interfaces/ITickLens.sol";
 
 /// @title Tick Lens contract
@@ -41,61 +38,6 @@ contract TickLens is ITickLens {
                     liquidityGross: liquidityGross
                 });
             }
-        }
-    }
-
-    /// @inheritdoc ITickLens
-    function getPopulatedTicks(address pool, int24 tick, uint256 maxBitmaps)
-        public
-        view
-        override
-        returns (PopulatedTick[] memory populatedTicks)
-    {
-        // fetch bitmaps
-        int24 tickSpacing = ICLPool(pool).tickSpacing();
-        int16 bitmapIndex = int16((tick / tickSpacing) >> 8);
-        maxBitmaps = Math.min(maxBitmaps, uint256(type(int16).max - bitmapIndex) + 1);
-
-        // get all `maxBitmaps` starting from the given tick's bitmap index
-        uint256 bitmap;
-        uint256 numberOfPopulatedTicks;
-        uint256[] memory bitmaps = new uint256[](maxBitmaps);
-        for (uint256 j = 0; j < maxBitmaps; j++) {
-            // calculate the number of populated ticks
-            bitmap = ICLPool(pool).tickBitmap(bitmapIndex++);
-            numberOfPopulatedTicks += countSetBits(bitmap);
-            bitmaps[j] = bitmap;
-        }
-
-        // fetch populated tick data
-        populatedTicks = new PopulatedTick[](numberOfPopulatedTicks);
-
-        int24 populatedTick;
-        int24 tickBitmapIndex;
-        for (uint256 j = 0; j < maxBitmaps; j++) {
-            bitmap = bitmaps[j];
-            tickBitmapIndex = bitmapIndex + int16(j);
-            for (uint256 i = 0; i < 256; i++) {
-                if (bitmap & (1 << i) > 0) {
-                    populatedTick = ((tickBitmapIndex << 8) + int24(i)) * tickSpacing;
-
-                    (uint128 liquidityGross, int128 liquidityNet,,,,,,,,) = ICLPool(pool).ticks(populatedTick);
-
-                    populatedTicks[--numberOfPopulatedTicks] = PopulatedTick({
-                        tick: populatedTick,
-                        sqrtRatioX96: TickMath.getSqrtRatioAtTick(populatedTick),
-                        liquidityNet: liquidityNet,
-                        liquidityGross: liquidityGross
-                    });
-                }
-            }
-        }
-    }
-
-    function countSetBits(uint256 bitmap) private pure returns (uint256 count) {
-        while (bitmap != 0) {
-            bitmap &= (bitmap - 1);
-            count++;
         }
     }
 }
--- contracts/periphery/test/TickLensTest.sol
+++ contracts/periphery/test/TickLensTest.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.5.0;
 pragma abicoder v2;
 
+import {Math} from "@openzeppelin/contracts/math/Math.sol";
+
+import {TickMath} from "../../core/libraries/TickMath.sol";
 import "contracts/core/interfaces/ICLPool.sol";
 import "../lens/TickLens.sol";
 
@@ -21,5 +24,66 @@ contract TickLensTest is TickLens {
         uint256 gasBefore = gasleft();
         getPopulatedTicks(pool, tick, maxCount);
         return gasBefore - gasleft();
+    }
+
+    /// @notice Get all the tick data for the populated ticks from multiple words, starting
+    /// at the word where the given pool's tick is located
+    /// @dev    Helper function used to estimate max number of bitmaps to be fetched
+    /// @param pool The address of the pool for which to fetch populated tick data
+    /// @param tick The tick from which to fetch the first tick bitmap index. The remaining bitmaps are
+    /// consecutively fetched starting from the first bitmap.
+    /// @param maxBitmaps Maximum number of bitmaps from which all the populated ticks will be fetched
+    /// @return populatedTicks An array of tick data for the given word in the tick bitmap
+    function getPopulatedTicks(address pool, int24 tick, uint256 maxBitmaps)
+        public
+        view
+        returns (PopulatedTick[] memory populatedTicks)
+    {
+        // fetch bitmaps
+        int24 tickSpacing = ICLPool(pool).tickSpacing();
+        int16 startBitmapIndex = int16((tick / tickSpacing) >> 8);
+        maxBitmaps = Math.min(maxBitmaps, uint256(type(int16).max - startBitmapIndex) + 1);
+
+        // get all `maxBitmaps` starting from the given tick's bitmap index
+        uint256 bitmap;
+        uint256 numberOfPopulatedTicks;
+        uint256[] memory bitmaps = new uint256[](maxBitmaps);
+        for (uint256 j = 0; j < maxBitmaps; j++) {
+            // calculate the number of populated ticks
+            bitmap = ICLPool(pool).tickBitmap(startBitmapIndex + int16(j));
+            numberOfPopulatedTicks += countSetBits(bitmap);
+            bitmaps[j] = bitmap;
+        }
+
+        // fetch populated tick data
+        populatedTicks = new PopulatedTick[](numberOfPopulatedTicks);
+
+        int24 populatedTick;
+        int24 tickBitmapIndex;
+        for (uint256 j = 0; j < maxBitmaps; j++) {
+            bitmap = bitmaps[j];
+            tickBitmapIndex = startBitmapIndex + int16(j);
+            for (uint256 i = 0; i < 256; i++) {
+                if (bitmap & (1 << i) > 0) {
+                    populatedTick = ((tickBitmapIndex << 8) + int24(i)) * tickSpacing;
+
+                    (uint128 liquidityGross, int128 liquidityNet,,,,,,,,) = ICLPool(pool).ticks(populatedTick);
+
+                    populatedTicks[--numberOfPopulatedTicks] = PopulatedTick({
+                        tick: populatedTick,
+                        sqrtRatioX96: TickMath.getSqrtRatioAtTick(populatedTick),
+                        liquidityNet: liquidityNet,
+                        liquidityGross: liquidityGross
+                    });
+                }
+            }
+        }
+    }
+
+    function countSetBits(uint256 bitmap) private pure returns (uint256 count) {
+        while (bitmap != 0) {
+            bitmap &= (bitmap - 1);
+            count++;
+        }
     }
 }
